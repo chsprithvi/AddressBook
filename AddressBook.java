674d94640c420c9b881049bf7f675cbf50468d69
chsprithvi <chsprithvi@gmail.com>
2020-09-18 13:00:10 +0530

    [Siidhartha]Added:Function to add contact details

--- AddressBook.java
+++ AddressBook.java
@@ -1,15 +1,29 @@
 import java.util.*;
 class AddressBook{
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook");
+        Map<String, List<Object>> addressBook = new TreeMap<>();
+        addPerson(addressBook);
     }
     public static void addPerson(Map<String, List<Object>> addressBook) {
         String firstName="John";
         String lastName="Wick";
-        String address="USA";
+        String address="Universal Studios";
         String city="New York";
-        String state="PA state";
-        Integer zip=123;
-        Integer phone=98765;
+        String state="Philadelphia";
+        Integer zip=12345;
+        Integer phone=987654321;
+
+        List<Object> ContactPerson = new ArrayList<>();
+        ContactPerson.add(firstName);
+        ContactPerson.add(lastName);
+        ContactPerson.add(address);
+        ContactPerson.add(city);
+        ContactPerson.add(state);
+        ContactPerson.add(zip);
+        ContactPerson.add(phone);
+
+        String name = firstName + " " + lastName;
+        addressBook.put(name, ContactPerson);
+        System.out.println(addressBook);
     }
 }
