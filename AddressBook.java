c1791a92faab8405f2bceed3b18ebd5913f57a81
chsprithvi <chsprithvi@gmail.com>
2020-09-18 12:51:10 +0530

    [Siidhartha]Added:created a contact person in AddressBook

---
+++ AddressBook.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class AddressBook{
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook");
+    }
+    public static void addPerson(Map<String, List<Object>> addressBook) {
+        String firstName="John";
+        String lastName="Wick";
+        String address="USA";
+        String city="New York";
+        String state="PA state";
+        Integer zip=123;
+        Integer phone=98765;
+    }
+}
